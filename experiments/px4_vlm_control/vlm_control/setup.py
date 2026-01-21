c55c35b0a0034b714937235cc6d3466f923b2347
Liam Ostrander <liamostrander0@gmail.com>
2026-01-21 17:41:15 +0000

    added script for controlling drone based on semantic reasoning (arrow direction)

--- experiments/px4_vlm_control/vlm_control/setup.py
+++ experiments/px4_vlm_control/vlm_control/setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'vlm_control = vlm_control.vlm_control:main'
+            'vlm_control_arrows = vlm_control.vlm_control_arrows:main'
         ],
     },
 )
